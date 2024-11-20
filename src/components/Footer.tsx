df81e8e080cff158f93e58b85d4b1aa3122eaea1
glglgl1711 <gunhee0906@gmail.com>
2024-11-20 14:59:21 +0900

    주석 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
+// styled-components 를 활용한 css 설계
+// 1. npm install styled-components
+// 2. import styled from "styled-components";
+// 3. 아래와 같이 css 구성
+
 export default function Footer () {
 
     return(
